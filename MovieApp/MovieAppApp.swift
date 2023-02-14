902d50944de336821716eb072b09b1778ee3a4f1
tesddev <tesleem.amuda@gmail.com>
2023-02-14 11:49:52 +0100

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by GIGL iOS on 14/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
