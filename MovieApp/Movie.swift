cfdc6bded0f41379b7fbb26d842cd28505745949
tesddev <tesleem.amuda@gmail.com>
2023-02-14 12:00:43 +0100

    add movie model

--- MovieApp/Movie.swift
+++ MovieApp/Movie.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String
+}
+
+extension Movie {
+    static var mock: Movie {
+        Movie(
+            id: 507086,
+            title: "Jurassic World Dominion",
+            overview: "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history’s most fearsome creatures.",
+            posterPath: "/w4c0GTpmEQ1CZQNHndTv2PPgf2p.jpg"
+        )
+    }
+}
