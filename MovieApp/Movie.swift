748d8050df86d097ae628c0613cfed7b71fa6dfc
tesddev <tesleem.amuda@gmail.com>
2023-02-14 13:22:42 +0100

    correct url

--- MovieApp/Movie.swift
+++ MovieApp/Movie.swift
@@ -23,7 +23,7 @@ extension Movie {
             id: 507086,
             title: "Jurassic World Dominion",
             overview: "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history’s most fearsome creatures.",
-            posterPath: "/w4c0GTpmEQ1CZQNHndTv2PPgf2p.jpg"
+            posterPath: "w4c0GTpmEQ1CZQNHndTv2PPgf2p.jpg"
         )
     }
 }
