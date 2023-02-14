60be890f5ffbf3a6793bfa9abfacccd300a84ca8
tesddev <tesleem.amuda@gmail.com>
2023-02-14 13:28:39 +0100

    add and resize image of screen

--- MovieApp/MovieView.swift
+++ MovieApp/MovieView.swift
@@ -12,14 +12,24 @@ struct MovieView: View {
     
     var body: some View {
         HStack {
-            AsyncImage(url: movie.posterURL)
+            AsyncImage(url: movie.posterURL) { image in
+                image
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } placeholder: {
+                
+            }
+            .frame(width: 80)
+            
             VStack(alignment: .leading){
                 Text(movie.title)
                     .font(.headline)
                 Text(movie.overview)
+                    .lineLimit(4)
             }
             .padding()
         }
+        .padding()
     }
 }
 
