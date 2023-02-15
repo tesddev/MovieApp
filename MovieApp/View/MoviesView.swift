e85cf2a1b0e3a7368ec88350da4daed07dde0507
tesddev <tesleem.amuda@gmail.com>
2023-02-15 22:31:14 +0100

    use navigationLink to navigate to detail view through cells

--- MovieApp/View/MoviesView.swift
+++ MovieApp/View/MoviesView.swift
@@ -32,21 +32,25 @@ struct MoviesView: View {
     func listOf(movies: [Movie]) -> some View {
         if !movies.isEmpty {
             List(movies) { movie in
-                HStack {
-                    AsyncImage(url: movie.posterURL) { image in
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                    } placeholder: {
-                        ProgressView()
-                    }
-                    .frame(width: 80)
-                    
-                    VStack(alignment: .leading){
-                        Text(movie.title)
-                            .font(.headline)
-                        Text(movie.overview)
-                            .lineLimit(4)
+                NavigationLink {
+                    MovieDetailView(movie: movie)
+                } label: {
+                    HStack {
+                        AsyncImage(url: movie.posterURL) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 80)
+                        
+                        VStack(alignment: .leading){
+                            Text(movie.title)
+                                .font(.headline)
+                            Text(movie.overview)
+                                .lineLimit(4)
+                        }
                     }
                 }
                 .padding()
