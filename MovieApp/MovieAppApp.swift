d21ebffc571f53906a38640c3e1464f6658e55c7
tesddev <tesleem.amuda@gmail.com>
2023-02-15 22:24:30 +0100

    embed topmost view in navigation view

--- MovieApp/MovieAppApp.swift
+++ MovieApp/MovieAppApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct MovieAppApp: App {
     var body: some Scene {
         WindowGroup {
-            MoviesView()
+            NavigationView {
+                MoviesView()
+            }
         }
     }
 }
